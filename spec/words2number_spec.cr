55bd759c717f03467306471d0c5f1dc760843cee
jasonm23 <jasonm23@gmail.com>
2020-08-30 13:53:46 +0000

    Stub cli for words2number

---
+++ spec/words2number_spec.cr
@@ -0,0 +1,13 @@
+require "./spec_helper"
+
+require "./spec_cli_helper"
+
+require "../src/words2number"
+
+describe "words2number CLI" do
+
+  it "returns 0 on normal exit" do
+
+  end
+
+end
---
+++ src/words2number.cr
@@ -0,0 +1,4 @@
+require "./words_to_numbers"
+
+# CLI tool to run words to numbers
+
